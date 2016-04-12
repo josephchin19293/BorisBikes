db4e3a3497a973a7044957fe1eb6b425172fe419
Ruta Baltiejute <Ruta@Rutas-MacBook-Pro.local>
2016-04-12 16:26:49 +0100

    Challenge 13 completed

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,11 +1,23 @@
 class DockingStation
+  attr_reader :bike
+
   def release_bike
-    Bike.new
+    fail "No bikes available!" unless @bike
+    @bike
+  end
+
+  def dock(bike)
+    fail "The dock is full!" if @bike
+    @bike = bike
   end
 end
 
 class Bike
   def working?
-    true
+  end
+
+  def docked?
   end
 end
+
+# require "./lib/docking_station.rb"
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -5,8 +5,36 @@ describe DockingStation do
     expect(subject).to respond_to :release_bike
   end
 
+=begin
   it 'gets a bike' do
     x = subject.release_bike
     expect(x).to be_working
+  end
+=end
+
+  it 'responds to dock with an argument' do
+    expect(subject).to respond_to(:dock).with(1).argument
+  end
+
+  it "responds to the method 'bike'" do
+    is_expected.to respond_to(:bike)
+  end
+
+  it 'docks a bike' do
+    x = Bike.new
+    expect(subject.dock(x)).to eq x
+  end
+
+  describe '#release_bike' do
+    it 'raises an error when no bikes' do
+      expect {subject.release_bike}.to raise_error "No bikes available!"
+    end
+  end
+
+  describe '#dock'do
+    it 'raises an error when there is a bike already docked' do
+      subject.dock(Bike.new)
+      expect {subject.dock Bike.new}.to raise_error "The dock is full!"
+    end
   end
 end
